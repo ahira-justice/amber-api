ce7fda0aa1563019763626838004fdc10d2e58ec
Ahira <justiceahira@gmail.com>
2021-05-06 12:46:38 +0100

    Add base entity

--- app/data/models.py
+++ app/data/models.py
@@ -1,11 +1,24 @@
-from sqlalchemy import Boolean, Column, Integer, String, LargeBinary
+from sqlalchemy import Boolean, Column, DateTime, Integer, String, LargeBinary
+
+from datetime import datetime
 
 from app.domain.database import Base
 
 
-class User(Base):
-    __tablename__ = "users"
+class BaseEntity(Base):
+
+    __abstract__ = True
+
     id = Column(Integer, primary_key=True, index=True)
+    created_on = Column(DateTime, default=datetime.utcnow, nullable=False)
+    updated_on = Column(DateTime, default=None, onupdate=datetime.utcnow, nullable=True)
+    is_deleted = Column(Boolean, default=False, nullable=False)
+
+
+class User(BaseEntity):
+
+    __tablename__ = "users"
+
     lname = Column(String, nullable=False)
     fname = Column(String, nullable=False)
     email = Column(String, unique=True, nullable=False, index=True)
