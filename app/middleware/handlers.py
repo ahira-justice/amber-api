6390a70a1dba642a54d21fd1efbe58f2095e1def
Ahira <justiceahira@gmail.com>
2021-05-17 22:52:19 +0100

    See 2b6850d6

--- app/middleware/handlers.py
+++ app/middleware/handlers.py
@@ -12,10 +12,6 @@ async def http_logging_middleware(request: Request, call_next):
 
     response: Response = await call_next(request)
 
-    # request_body = await request.body()
-    # request_body = request_body.decode("utf-8")
-    # logger.info(f"Request Body: {request_body}")
-
     process_time = time.time() - start_time
     logger.info(f"Finished running request '{request.method} > {request.url}' in {process_time} seconds")
     logger.info(f"Response Status Code: {response.status_code}")
