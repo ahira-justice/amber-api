d0104094910d01d9fcd4828f702290ffdecb427d
Ahira <justiceahira@gmail.com>
2021-06-16 21:36:32 +0100

    Remove multiple scores for one user on leaderboards

--- app/commonhelper/utils.py
+++ app/commonhelper/utils.py
@@ -2,7 +2,9 @@ import hashlib
 import os
 import uuid
 
-from typing import Tuple
+from typing import List, Tuple
+
+from app.data import models
 
 
 def generate_hash_and_salt(password: str) -> Tuple[bytes]:
@@ -20,3 +22,19 @@ def generate_hash_and_salt(password: str) -> Tuple[bytes]:
 
 def generate_reset_code() -> int:
     return uuid.uuid4()
+
+
+def remove_duplicates(games: List[models.Game]) -> List[models.Game]:
+    user_ids = [].append(games[0].user_id)
+    result = games.copy()
+
+    for index in range(1, len(games)):
+        game = games[index]
+
+        if game.user_id not in user_ids:
+            user_ids.append(game.user_id)
+
+        if game.user_id in user_ids:
+            result.pop(index)
+
+    return result
