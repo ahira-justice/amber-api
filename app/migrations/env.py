4133034549810c777a87113e864422e6ec9cd3e8
Ahira Adefokun <ahira.adefokun@teamapt.com>
2022-01-15 20:31:05 +0100

    Modify alembic logger setup to not disable existing loggers

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -15,7 +15,7 @@ config.set_main_option("sqlalchemy.url", SQLALCHEMY_DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
-fileConfig(config.config_file_name)
+fileConfig(config.config_file_name, disable_existing_loggers=False)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
