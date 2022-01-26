803e0d6a2906d81594e9393071c7f499f01e7bcf
Ahira Adefokun <ahira.adefokun@teamapt.com>
2022-01-26 14:47:42 +0100

    Setup loguru logging intercept

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, Request
 from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
+from loguru import logger
+from loguru_logging_intercept import setup_loguru_logging_intercept as logging_intercept
 
 from app.controllers.user_controller import controller as user_controller
 from app.controllers.game_controller import controller as game_controller
@@ -10,8 +12,12 @@ from app.domain.config import ENVIRONMENT, SQLALCHEMY_DATABASE_URL
 from app.domain.constants import ALEMBIC_INI_DIR, DOCS_URL, MIGRATIONS_DIR, OPEN_API_URL
 from app.exceptions.app_exceptions import AppDomainException
 from app.exceptions.handlers import exception_handler, app_exception_handler, validation_exception_handler
-from loguru import logger
 from app.middleware.handlers import http_logging_middleware
+
+
+logging_intercept(
+    modules=("uvicorn", "alembic", "sqlalchemy")
+)
 
 
 if (ENVIRONMENT != "TEST"):
