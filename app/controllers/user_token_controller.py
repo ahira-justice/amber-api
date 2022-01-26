e9a2cfa3775c14d8e942ea63963321cbb42c51ea
Ahira Adefokun <ahira.adefokun@teamapt.com>
2022-01-26 15:18:23 +0100

    Implement verify user token

---
+++ app/controllers/user_token_controller.py
@@ -0,0 +1,38 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm.session import Session
+
+from app.auth.bearer import BearerAuth
+from app.domain.constants import USER_TOKENS_URL
+from app.domain.database import get_db
+from app.dtos import error, user_token_dtos
+from app.services import user_token_service
+
+controller = APIRouter(
+    prefix=USER_TOKENS_URL,
+    tags=["User Tokens"]
+)
+
+
+@controller.post(
+    path="/verify",
+    dependencies=[Depends(BearerAuth())],
+    status_code=200,
+    responses={
+        200: {
+            "model": bool
+        },
+        401: {
+            "model": error.ErrorResponse
+        },
+        422: {
+            "model": error.ValidationErrorResponse
+        }
+    }
+)
+async def verify_user_token(
+    request: user_token_dtos.VerifyUserTokenRequest,
+    db: Session = Depends(get_db)
+):
+    """Verify user token"""
+
+    return user_token_service.verify_user_token(db, request)
